70cdc2ff1903d3e7fed9ecfde1f4124e6d30cc7c
victorwanderley1 <victor.wanderley1709@gmail.com>
2022-10-01 17:24:11 -0300

    init: Commit inicial. Arquivos base.

---
+++ src/main/java/wanderley/victor/introducaospringsecurityjwt/IntroducaoSpringsecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package wanderley.victor.introducaospringsecurityjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroducaoSpringsecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroducaoSpringsecurityJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wanderley/victor/introducaospringsecurityjwt/IntroducaoSpringsecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package wanderley.victor.introducaospringsecurityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroducaoSpringsecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
